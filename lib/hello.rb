528e4b2a61915b17f08750c06b8019b466f3d283
a lan <bluenowte97@gmail.com>
2018-11-07 00:27:49 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
 def hello_t
+  i = 0
 
-end
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
 
 # call your method here!
 
+```ruby
+["Tim", "Tom", "Jim"]
+```
+
+```ruby
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+```
